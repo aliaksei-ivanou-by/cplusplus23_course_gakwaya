bfe7ca48896b81f4b121d450fb1169b00c23337e
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2025-03-26 13:19:11 +0300

    Add Course Files

---
+++ FinalCode-Headers/10.Functions/10.Challenge_2/main.cpp
@@ -0,0 +1,14 @@
+/*
+    . ftxui calculator
+    . sfml apps
+*/
+
+#include "utilities.h"
+
+int main(int argc, char* argv[]){
+
+    //it_1::app();
+    //it_2::app();
+    it_3::app(argc, argv);
+    
+}
