791d69e436c26aee5b266dd57367842607c1eb9b
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2025-04-03 12:58:00 +0300

    Add 04 Variables And Datatypes And Operations - 02 Integer Modifiers Characters Text Auto Headers

---
+++ 04_Variables_And_Datatypes_And_Operations/02_Integer_Modifiers_Characters_Text_Auto_Headers/main.cpp
@@ -0,0 +1,15 @@
+#include <fmt/format.h>
+#include "utilities.h"
+
+int main() {
+    //braced_initialization();
+    //functional_initialization();
+    //assignment_initialization();
+    //integer_modifiers();
+    //floating_point_numbers();
+    //infinity_nan();
+    //bool_type();
+    //characters_and_text();
+    //collections_of_characters();
+    auto_keyword();
+}
